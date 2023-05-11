08454fbc73238da19a0b56de819ef2cda1c56c94
Huilson J. Lorenzi <hill.winterclaw@gmail.com>
2023-05-10 21:59:12 -0300

    Pasta alunos

---
+++ Alunos/Orientação a Objetos 2/Edson De Col/Edson_De_Col_Trabalho_Crud_Zoologico_Mongo/Edson_De_Col_Trabalho_Crud_Zoologico_Mongo/Edson_De_Col_Zoologico_Mongo/src/main/java/br/edu/utfpr/Classes/Profissional.java
@@ -0,0 +1,71 @@
+package br.edu.utfpr.Classes;
+
+import org.bson.types.ObjectId;
+
+public class Profissional {
+
+    private ObjectId _id;
+    private String nome;
+    private String cpf;
+    private String cargo;
+    private String cidade;
+    private String bairro;
+    private String rua;
+
+    public ObjectId getId() {
+        return _id;
+    }
+
+    public void setId(ObjectId _id) {
+        this._id = _id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+}
---
+++ Alunos/Orientação a Objetos 2/Edson De Col/Edson_De_Col_Trabalho_Crud_Zoologico_Mongo/Edson_De_Col_Trabalho_Crud_Zoologico_Mongo/Edson_De_Col_Zoologico_Mongo/src/main/java/br/edu/utfpr/Main/Main.java
@@ -0,0 +1,9 @@
+package br.edu.utfpr.Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+                
+        Menus menu = new Menus();
+    }
+}
